bd59ddf798df2c4a29bdeb4baab16d8352786962
labib3013 <alfarabyhd30@gmail.com>
2023-11-29 18:28:58 +0700

    feat()

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const halo = 'hello wordl'
 console.log(halo + 'Halo Dunia');
 console.log(halo)
 console.log('ini testing tambahan');
-console.log('ini testing tambahan 2');
+console.log('ini testing tambahan 24');
